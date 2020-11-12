7eb90a0ee633e1bd3f4a4d84300373e91a797435
stackoverprof <mail.errbint@gmail.com>
2020-11-12 09:55:38 +0700

    kuis, functs, check allow kuis, kuis taken

--- functions/index.js
+++ functions/index.js
@@ -1,56 +1,57 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-// const { log } = require('firebase-functions/lib/logger')
-
+const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors({origin: ['*']}));
+app.use(cors());
 
+// const { log } = require('firebase-functions/lib/logger')
+const admin = require('firebase-admin')
 admin.initializeApp()
+const db = admin.firestore()
 
-// exports.addAdminRole = functions.https.onCall((data, context) => {
-//     // get user and add custom claims
-//     return admin.auth().getUserByEmail(data.email).then(user => {
-//         return admin.auth().setCustomUserClaims(user.uid, {
-//             admin: true
-//         })
-//     }).then(() => {
-//         return {
-//             message: `Success! ${data.email} has been made an admin. YEAY!`
-//         }
-//     }).catch((err) => {
-//         return err;
-//     })
-// })
+const submit = (req, res) => {
+    const { answers, kuis, kuisID, userID } = req.body
 
-exports.corrector = functions.https.onRequest(async (req, res) => {
-    if(req.method !== "POST")return res.status(400).json({message: "Not Allowed"})
-    
-    let { answers, kuisID } = req.body
-
-    admin.firestore().collection('Kuis').doc(kuisID).collection('Answers').doc('kunci').get().then(function(doc) {
-        let kunciArr = []
-        kunciArr = doc.data().body
-
+    db.collection('Kuis').doc(kuisID).collection('Answers').doc('kunci').get().then(
+    async function(doc) {
+        let kunciArr = doc.data().body
         let hasilUser = []
-        kunciArr.forEach((kcr, i) => {
-            hasilUser.push(typeof answers[i] !== 'undefined' ? answers[i] === kcr : false)
+        let nilai = 0
+        
+        kunciArr.forEach((kunci, i) => {
+            hasilUser.push(typeof answers[i] !== 'undefined' ? answers[i] === kunci : false)
         })
         
-        let nilai = 0
-        hasilUser.forEach(hsl => {
-            if(hsl) nilai++
+        hasilUser.forEach(hasil => {
+            if(hasil) nilai++
         })
-
+        
         nilai = (nilai/kunciArr.length) * 100
+        
+        const isSaved = await db.collection('Profile').doc(userID).collection('Kuis').doc(kuisID).set({
+            topikID: kuis.topikID,
+            kuisID: kuisID,
+            namaKuis: kuis.Nama,
+            body: nilai,
+            answers: answers,
+            hasilUser: hasilUser
+         
+        }).then(() => {
+            return 'tersimpan'
+        }).catch(err => {
+            return `gagal menyimpan err : ${err}`
+        })
+        
 
         res.status(200).json({
+            
             message : "Hello World!",
             isi : req.body,
             kunci : kunciArr,
             hasilUser: hasilUser,
-            nilai: nilai
+            nilai: nilai,
+            userID : userID,
+            isSaved: isSaved
         })
     })
     .catch(err => {
@@ -58,16 +59,18 @@ exports.corrector = functions.https.onRequest(async (req, res) => {
             body : `failed with error : ${err}`
         })
     })
+}
 
-})
+const halo = (req, res) => {
+    res.status(200).json({
+        message : "Hello World!"
+    })
+}
+
+app.post("/submit", submit)
+
+app.get("/halo", halo)
 
 
+exports.api = functions.https.onRequest(app)
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
