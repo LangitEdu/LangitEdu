82ef80c1268c89ea07961e6d758fba63cbaccb24
zulfaza <32305083+zulfaza@users.noreply.github.com>
2020-11-14 01:38:02 +0700

    merge

--- src/component/App.js
+++ src/component/App.js
@@ -20,28 +20,33 @@ import Admin from "../Pages/Admin"
 import Kuis from "../Pages/Kuis"
 import KuisResult from "../Pages/Kuis/Result"
 import MultipleMenus from "../Pages/SimpleMenu"
-import Journey from "../Pages/Journey"
+import AdminJourney from "../Pages/AdminJourney"
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
+          
           <GuestOnlyRoute path={RouteName.register} component={Register} />
           <GuestOnlyRoute path={RouteName.login} component={Login} />
           <GuestOnlyRoute path={RouteName.forgetPassword} component={ForgotPassword} />
+          
+          <AdminRoute exact path={RouteName.admin} component={Admin} />
+          <AdminRoute exact path={RouteName.liatJourney} component={AdminJourney} />
+          
           <UserRoute exact path={RouteName.dashboard} component={Dashboard} />
           <UserRoute exact path={RouteName.editProfile} component={EditProfile} />
           <UserRoute exact path={RouteName.journey} component={Journey} />
           <UserRoute exact path={RouteName.topik} component={Topik} />
           <UserRoute exact path={RouteName.gotoTopik} component={Topik} />
           <UserRoute exact path={RouteName.listKomunitas} component={ListKomunitas} />
-          <AdminRoute exact path={RouteName.admin} component={Admin} />
-          <AdminRoute exact path={RouteName.liatJourney} component={Journey} />
           <UserRoute exact path={RouteName.kuis} component={Kuis} />
           <UserRoute exact path={RouteName.kuisresult} component={KuisResult} />
+          
           <Route exact path={RouteName.home} component={Home} />
           <Route exact path={RouteName.coba} component={MultipleMenus} />
+        
         </Switch>
       </Router>
     </AuthProvider>
