04968a0df3cd92e45aee4b730bfc2fa617c8a49e
zulfaza <32305083+zulfaza@users.noreply.github.com>
2020-11-15 02:43:16 +0700

    add edit stage

--- src/Pages/Admin/TambahSoal.js
+++ src/Pages/Admin/TambahSoal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Editor } from '@tinymce/tinymce-react'
 import OpsiItem from '../../component/TambahSoal/OpsiItem'
@@ -6,8 +6,9 @@ import { db, FieldValue } from '../../config/Firebase'
 
 export default function TambahSoal() {
     const [KuisData, setKuisData] = useState()
+    const [OnEdit, setOnEdit] = useState(false)
     const {uid} = useParams()
-    const [Loading, setLoading] = useState()
+    const [Loading, setLoading] = useState()    
     const defaultOpsiList = ['A','B','C','D','E']
     const [isiOpsiA, setIsiOpsiA] = useState('')
     const [isiOpsiB, setIsiOpsiB] = useState('')
@@ -15,16 +16,16 @@ export default function TambahSoal() {
     const [isiOpsiD, setIsiOpsiD] = useState('')
     const [isiOpsiE, setIsiOpsiE] = useState('')
     const [BodySoal, setBodySoal] = useState()
+    const [Jawaban, setJawaban] = useState()
     const [BodyPembahasan, setBodyPembahasan] = useState()
-    const arrIsiOpsi = [isiOpsiA, isiOpsiB, isiOpsiC, isiOpsiD, isiOpsiE]
-    const arrSetIisiOpsi = [setIsiOpsiA, setIsiOpsiB, setIsiOpsiC, setIsiOpsiD, setIsiOpsiE]
+    let arrIsiOpsi = useMemo(()=>{return [isiOpsiA, isiOpsiB, isiOpsiC, isiOpsiD, isiOpsiE]},[isiOpsiA, isiOpsiB, isiOpsiC, isiOpsiD, isiOpsiE])
+    let arrSetIisiOpsi = useMemo(() => {return [setIsiOpsiA, setIsiOpsiB, setIsiOpsiC, setIsiOpsiD, setIsiOpsiE]}, [setIsiOpsiA, setIsiOpsiB, setIsiOpsiC, setIsiOpsiD, setIsiOpsiE])
     const [ListOpsi, setListOpsi] = useState(['A','B'])
     const [ItemOpsi, setItemOpsi] = useState(ListOpsi.map((type,i)=>{
         return <OpsiItem type={type} onEditorChange={arrSetIisiOpsi[i]} key={type}/>
     }))
 
     const NoRef = useRef()
-    const JawababRef = useRef()
 
     const handleTambahOpsi = ()=>{
         const currentOpsiList = ListOpsi
@@ -69,7 +70,7 @@ export default function TambahSoal() {
             pembahasan :BodyPembahasan
         }
         const answer = {
-            answer : JawababRef.current.value,
+            answer : Jawaban,
             id : NoRef.current.value-1
         }
         const SoalRef = db.collection('Kuis').doc(uid)
@@ -99,11 +100,8 @@ export default function TambahSoal() {
                         listQuestion : FieldValue.arrayUnion({id:nomer})
                     }).then(()=>{
                         console.log('Berhasil update');
-                        setBodySoal()
-                        setBodyPembahasan()
-                        NoRef.current.value = ''
-                        JawababRef.current.value = ''
                         setLoading(false)
+                        return resetForm
                     }).catch((err)=>{
                         console.log(err);
                         setLoading(false)
@@ -115,6 +113,102 @@ export default function TambahSoal() {
             setLoading(false)
         })
         
+    }
+    const handleSubmitEdit = (e)=>{
+        e.preventDefault()
+        console.log(e);
+    }
+    const resetForm = ()=>{
+        setOnEdit(false)
+        setBodySoal('')
+        setBodyPembahasan('')
+        setIsiOpsiA('')
+        setIsiOpsiB('')
+        setIsiOpsiC('')
+        setIsiOpsiD('')
+        setIsiOpsiE('')
+        setJawaban('')
+        setListOpsi(['A','B'])
+        setItemOpsi(ListOpsi.map((type,i)=>{
+            return <OpsiItem type={type} onEditorChange={arrSetIisiOpsi[i]} key={type}/>
+        }))
+        NoRef.current.value = 1
+    }
+    const handleEditSoal = async (e)=>{
+        setOnEdit(true)
+        const idSoal = e.target.dataset.id
+        db.collection('Kuis')
+            .doc(uid)
+            .collection('Questions')
+            .doc(idSoal)
+            .get()
+            .then(res=>{
+                const {body, options, pembahasan} = res.data();
+                setBodySoal(body)
+                setBodyPembahasan(pembahasan)
+                let resListOpsi = []
+
+                options.forEach(data=>{
+                    resListOpsi.push(data.type)
+                    
+                    switch (data.type) {
+                        case 'A':
+                            setIsiOpsiA(data.body)
+                            break;
+                        case 'B':
+                            setIsiOpsiB(data.body)
+                            break;
+                        case 'C':
+                            setIsiOpsiC(data.body)
+                            break;
+                        case 'D':
+                            setIsiOpsiD(data.body)
+                            break;
+                        default:
+                            setIsiOpsiE(data.body)
+                            break;
+                    }
+                    setListOpsi(resListOpsi)
+                })
+                const excludeOpsi = defaultOpsiList.filter((data)=>{
+                    let diff = true;
+                    for (let i = 0; i < resListOpsi.length; i++) {
+                        if(data === resListOpsi[i]){
+                            diff= false
+                        }
+                        
+                    }
+                    return diff
+                })
+                excludeOpsi.forEach(data=>{
+                    switch (data) {
+                        case 'A':
+                            setIsiOpsiA('')
+                            break;
+                        case 'B':
+                            setIsiOpsiB('')
+                            break;
+                        case 'C':
+                            setIsiOpsiC('')
+                            break;
+                        case 'D':
+                            setIsiOpsiD('')
+                            break;
+                        default:
+                            setIsiOpsiE('')
+                            break;
+                    }
+                })
+                db.collection('Kuis')
+                    .doc(uid)
+                    .collection('Answers')
+                    .doc('kunci')
+                    .get()
+                    .then(res=>{
+                        setJawaban(res.data().body[idSoal].answer)
+                    })
+                NoRef.current.value = parseInt(idSoal)+1
+            })
     }
 
     const handleHapusSoal = async(e)=>{
@@ -166,7 +260,12 @@ export default function TambahSoal() {
                 setLoading(false)
             })
     }
-
+    useEffect(()=>{
+        setItemOpsi(ListOpsi.map((type,i)=>{
+            return <OpsiItem type={type} value={arrIsiOpsi[i]} onEditorChange={arrSetIisiOpsi[i]} key={type}/>
+        }))
+    }, [ListOpsi, arrSetIisiOpsi, arrIsiOpsi])
+    
     useEffect(() => {
         const unsub = db.collection('Kuis').doc(uid).onSnapshot(doc=>{
             setKuisData(doc.data())
@@ -180,10 +279,19 @@ export default function TambahSoal() {
                 <div className="col-md-8">
                     <div className="card">
                         <div className="card-header">
-                            <h4 className="mb-0">Tambah Soal</h4>
+                            <h4 className="mb-0">
+                                {OnEdit ? 
+                                <>
+                                <span className="mr-3" >Edit Soal</span>
+                                <button className="btn btn-success" onClick={resetForm} >Cancel</button>
+                                </>
+                                : 
+                                'Tambah Soal'
+                                }
+                            </h4>
                         </div>
                         <div className="card-body">
-                            <form onSubmit={handleBuatSoal}>
+                            <form onSubmit={ OnEdit ? handleSubmitEdit : handleBuatSoal}>
                                 <div className="form-group">
                                     <label htmlFor="no ">No</label>
                                     <input ref={NoRef} className="form-control" type="number" min='1' />
@@ -205,9 +313,9 @@ export default function TambahSoal() {
                                 <button className="btn btn-warning" type="button" onClick={handleHapusOpsi} disabled={Loading} >Hapus Opsi</button>
                                 <div className="form-group">
                                     <label htmlFor="Jawaban">Jawaban</label>
-                                    <select ref={JawababRef} className="form-control">
+                                    <select value={Jawaban} onChange={(e)=>{setJawaban(e.target.value)}} className="form-control">
                                         {ListOpsi.map(type=>{
-                                            return <option key={type}>{type}</option>
+                                            return <option key={type} value={type} >{type}</option>
                                         })}
                                     </select>
                                 </div>
@@ -235,12 +343,19 @@ export default function TambahSoal() {
                         </div>
                         <ul className="list-group list-group-flush">
                             {KuisData && KuisData.listQuestion && 
+                            KuisData.listQuestion.sort((a,b)=>{
+                                if(parseInt(a.id) < parseInt(b.id)){
+                                    return -1
+                                }else{
+                                    return 1
+                                }
+                            }) &&
                             KuisData.listQuestion.map(data=>{
                                 return (
                                 <li className="list-group-item d-flex justify-content-between" key={data.id} >
                                     Soal #{parseInt(data.id)+1}
                                     <div>
-                                        <button type="buttom" className="btn btn-success mr-3" data-id={data.id} disabled={Loading} >Edit</button>
+                                        <button type="buttom" className="btn btn-success mr-3" data-id={data.id} disabled={Loading} onClick={handleEditSoal} >Edit</button>
                                         <button type="buttom" className="btn btn-danger" data-id={data.id} onClick={handleHapusSoal} disabled={Loading} >Hapus</button>
                                     </div>
                                 </li>
