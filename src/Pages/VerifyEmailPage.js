c47cf3e8f35c1e019b77af3fe78986ea75f212ac
zulfaza <32305083+zulfaza@users.noreply.github.com>
2020-11-20 07:34:54 +0700

    benerin verify page

--- src/Pages/VerifyEmailPage.js
+++ src/Pages/VerifyEmailPage.js
@@ -21,12 +21,12 @@ export default function VerifyEmailPage(props) {
 
     return (
         <div className="container d-flex align-items-center justify-content-center flex-column vh-100">
-            <h1>{Sukses ? 'Error' :`Verifikasi Email Berhasil`}</h1>
+            <h1>{Sukses ? `Verifikasi Email Berhasil` : 'Error'}</h1>
             <div className="card w-75 mt-3 p-md-3">
                 <div className="card-body text-center">
                     <p className="mb-3" style={{fontSize:'1.5rem'}}>{`Silahkan menuju ${currentUser ? 'Beranda' : 'Login page'}`}</p>
                     {currentUser ?
-                    <Link className="btn btn-primary" to={RouteName.dashboard}>Beranda</Link>
+                    <a className="btn btn-primary" href={RouteName.dashboard}>Beranda</a>
                     :
                     <Link className="btn btn-primary" to={RouteName.login}>Login</Link>
                     }
